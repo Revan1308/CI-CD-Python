675a0c7cab3d840b32ea7b03b1ac49ab01963351
Loïc <frenzel.loic57@gmail.com>
2026-06-23 10:14:20 +0200

    correction

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from counter import add_one_to_count, remove_one_to_count
 
-def main(): 
+def main():
     count = add_one_to_count(0)
     print('Affichage du compteur après ajout : ' + str(count))
     count = remove_one_to_count(1)
--- counter.py
+++ counter.py
@@ -2,7 +2,7 @@ def add_one_to_count(count):
     count = count + 1
     return count
 
-def remove_one_to_count(count): 
-    if (count > 0):
+def remove_one_to_count(count):
+    if count > 0:
         count = count - 1
     return count
